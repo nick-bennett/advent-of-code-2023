2a6792311c5c53ea7446ab94393d8f9091b741f9
nick-bennett <nbennett10@cnm.edu>
2023-12-03 03:17:30 -0700

    Clean up.

--- src/main/java/com/nickbenn/adventofcode/day3/GearRatio.java
+++ src/main/java/com/nickbenn/adventofcode/day3/GearRatio.java
@@ -3,23 +3,25 @@ package com.nickbenn.adventofcode.day3;
 import com.nickbenn.adventofcode.util.DataSource;
 import com.nickbenn.adventofcode.util.Defaults;
 import java.io.IOException;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class GearRatio {
 
-  private static final char EMPTY_SPACE = '.';
-  private static final char GEAR_SYMBOL = '*';
+  private static final Pattern EXTRACTOR = Pattern.compile("(\\*)|([^\\d.*])|(\\d+)");
 
-  private final Map<Integer, Map<Integer, String>> partNumbers;
-  private final Set<Location> gears;
+  private final List<Map<Integer, String>> numbers;
+  private final List<BitSet> gears;
+  private final List<BitSet> symbols;
 
   public GearRatio() throws IOException {
     this(Defaults.INPUT_FILE);
@@ -31,130 +33,99 @@ public class GearRatio {
         .setContext(getClass())
         .build()
         .lines()) {
-      char[][] schematic = lines
-          .map(String::toCharArray)
-          .toArray(char[][]::new);
-      partNumbers = new TreeMap<>();
-      gears = new TreeSet<>();
-      analyzeSchematic(schematic);
+      numbers = new ArrayList<>();
+      gears = new ArrayList<>();
+      symbols = new ArrayList<>();
+      analyzeSchematic(lines);
     }
   }
 
   public static void main(String[] args) throws IOException {
     GearRatio ratio = new GearRatio();
-    System.out.println(new GearRatio().sumPartNumbers());
-    System.out.println(new GearRatio().sumGearRatios());
+    System.out.println(ratio.sumPartNumbers());
+    System.out.println(ratio.sumGearRatios());
   }
 
   public int sumPartNumbers() {
-    return partNumbers
-        .values()
-        .stream()
-        .flatMap((map) -> map.values().stream())
-        .mapToInt(Integer::parseInt)
-        .sum();
+    int sum = 0;
+    for (int rowIndex = 0; rowIndex < this.numbers.size(); rowIndex++) {
+      Map<Integer, String> numbers = this.numbers.get(rowIndex);
+      List<BitSet> symbols = this.symbols.subList(
+          Math.max(rowIndex - 1, 0), Math.min(rowIndex + 2, this.symbols.size()));
+      sum += numbers
+          .entrySet()
+          .stream()
+          .filter((entry) -> {
+            int start = entry.getKey();
+            int end = start + entry.getValue().length();
+            return symbols
+                .stream()
+                .flatMapToInt(BitSet::stream)
+                .anyMatch((pos) -> pos >= start - 1 && pos < end + 1);
+          })
+          .map(Entry::getValue)
+          .mapToInt(Integer::parseInt)
+          .sum();
+    }
+    return sum;
   }
 
-  public long sumGearRatios() {
-    return gears
-        .stream()
-        .map((gear) -> {
-          int gearRow = gear.row();
-          int gearColumn = gear.column();
-          return partNumbers
-              .entrySet()
+  public int sumGearRatios() {
+    int sum = 0;
+    for (int rowIndex = 0; rowIndex < this.gears.size(); rowIndex++) {
+      BitSet gears = this.gears.get(rowIndex);
+      List<Map<Integer, String>> numbers = this.numbers.subList(
+          Math.max(rowIndex - 1, 0), Math.min(rowIndex + 2, this.numbers.size()));
+      sum += gears
+          .stream()
+          .boxed()
+          .map((pos) -> numbers
               .stream()
-              .skip(Math.max(gearRow - 1, 0))
-              .takeWhile((entry) -> entry.getKey() <= gearRow + 1)
-              .flatMap((entry) -> entry.getValue().entrySet().stream())
+              .map(Map::entrySet)
+              .flatMap(Set::stream)
               .filter((entry) -> {
-                int col = entry.getKey();
-                String partNumberWord = entry.getValue();
-                int partNumberLength = partNumberWord.length();
-                return col <= gearColumn + 1 && col + partNumberLength > gearColumn - 1;
+                int start = entry.getKey();
+                int end = start + entry.getValue().length();
+                return pos >= start - 1 && pos < end + 1;
               })
               .map(Entry::getValue)
-              .collect(Collectors.toList());
-        })
-        .filter((numbers) -> numbers.size() == 2)
-        .mapToLong((numbers) -> numbers
-            .stream()
-            .mapToLong(Long::parseLong)
-            .reduce(1L, (a, b)-> a * b)
-        )
-        .sum();
+              .collect(Collectors.toList())
+          )
+          .filter((nums) -> nums.size() == 2)
+          .mapToInt((nums) -> nums
+              .stream()
+              .mapToInt(Integer::parseInt)
+              .reduce(1, (a, b) -> a * b)
+          )
+          .sum();
+    }
+    return sum;
   }
 
-  private void analyzeSchematic(char[][] schematic) {
-    StringBuilder builder = new StringBuilder();
-    for (int rowIndex = 0; rowIndex < schematic.length; rowIndex++) {
-      Map<Integer, String> rowPartNumbers = new TreeMap<>();
-      partNumbers.put(rowIndex, rowPartNumbers);
-      boolean inNumber = false;
-      boolean symbolAdjacent = false;
-      int partNumberStart = -1;
-      builder.setLength(0);
-      for (int colIndex = 0; colIndex < schematic[rowIndex].length; colIndex++) {
-        char c = schematic[rowIndex][colIndex];
-        if (Character.isDigit(c)) {
-          builder.append(c);
-          symbolAdjacent |= isSymbolAdjacent(schematic, rowIndex, colIndex, inNumber);
-          if (!inNumber) {
-            partNumberStart = colIndex;
-            inNumber = true;
-          }
-        } else {
-          if (inNumber) {
-            if (symbolAdjacent) {
-              rowPartNumbers.put(partNumberStart, builder.toString());
+  private void analyzeSchematic(Stream<String> lines) {
+    lines.forEach((line) -> {
+      Map<Integer, String> numbers = new TreeMap<>();
+      BitSet symbols = new BitSet();
+      BitSet gears = new BitSet();
+      this.numbers.add(numbers);
+      this.symbols.add(symbols);
+      this.gears.add(gears);
+      Matcher matcher = EXTRACTOR.matcher(line);
+      matcher
+          .results()
+          .forEach((result) -> {
+            int start = result.start();
+            String numberWord = result.group(3);
+            if (numberWord != null) {
+              numbers.put(start, numberWord);
+            } else {
+              symbols.set(start);
+              if (result.group(1) != null) {
+                gears.set(start);
+              }
             }
-            inNumber = false;
-            symbolAdjacent = false;
-            builder.setLength(0);
-          }
-          if (c == GEAR_SYMBOL) {
-            gears.add(new Location(rowIndex, colIndex));
-          }
-        }
-      }
-      if (inNumber && symbolAdjacent) {
-        rowPartNumbers.put(partNumberStart, builder.toString());
-      }
-    }
-  }
-
-  private boolean isSymbolAdjacent(char[][] schematic, int row, int column, boolean rightOnly) {
-    boolean result = false;
-    int startRow = row - ((row > 0) ? 1 : 0);
-    int endRow = row + ((row < schematic.length - 1) ? 1 : 0);
-    int startCol = rightOnly
-        ? column + 1
-        : column - ((column > 0) ? 1 : 0);
-    int endCol = column + ((column < schematic[row].length - 1) ? 1 : 0);
-    outer:
-    for (int rowIndex = startRow; rowIndex <= endRow; rowIndex++) {
-      for (int colIindex = startCol; colIindex <= endCol; colIindex++) {
-        char c = schematic[rowIndex][colIindex];
-        if (c != EMPTY_SPACE && !Character.isDigit(c)) {
-          result = true;
-          break outer;
-        }
-      }
-    }
-    return result;
-  }
-
-  private record Location(int row, int column) implements Comparable<Location> {
-
-    private static final Comparator<Location> COMPARATOR = Comparator
-        .comparing(Location::row)
-        .thenComparing(Location::column);
-
-    @Override
-    public int compareTo(@SuppressWarnings("NullableProblems") Location other) {
-      return COMPARATOR.compare(this, other);
-    }
-
+          });
+    });
   }
 
 }
