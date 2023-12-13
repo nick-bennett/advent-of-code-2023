70e18f1bdfec4518ff1dbe55c5b3f501da09054a
nick-bennett <nbennett10@cnm.edu>
2023-12-12 19:18:10 -0700

    Prep day 13.

--- src/test/java/com/nickbenn/adventofcode/day12/HotSpringsTest.java
+++ src/test/java/com/nickbenn/adventofcode/day12/HotSpringsTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.IOException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 class HotSpringsTest {
 
---
+++ src/test/java/com/nickbenn/adventofcode/day13/Day13Test.java
@@ -0,0 +1,17 @@
+package com.nickbenn.adventofcode.day13;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeEach;
+
+class Day13Test {
+
+  Day13 day13;
+
+  @BeforeEach
+  void setUp() throws IOException {
+    day13 = new Day13();
+  }
+
+}
