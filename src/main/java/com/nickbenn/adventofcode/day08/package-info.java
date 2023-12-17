a838e172b3f7d9633d2535085ea0527ac4c71fbe
nick-bennett <nbennett10@cnm.edu>
2023-12-16 17:19:49 -0700

    Text tweaks.

--- src/main/java/com/nickbenn/adventofcode/day08/package-info.java
+++ src/main/java/com/nickbenn/adventofcode/day08/package-info.java
@@ -1,8 +1,8 @@
 /**
  * Count the number of steps required to traverse a single path between an initial node and a
- * terminal node in a selected graph; then, compute the number of steps for multiple parallel
+ * terminal node in a selected graph; then compute the number of steps for multiple parallel
  * traversals, all starting at the same time, and all ending on a terminal node at the same time
- * (potentially after passing through one or more potential terminal nodes along the path).
+ * (potentially after passing through one or more potential terminal nodes along the way).
  *
  * @see <a href="https://adventofcode.com/2023/day/8">"Day 8: Haunted Wasteland"</a>.
  */
